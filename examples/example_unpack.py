008c7cf24e2b805a0d64af8b4548ab2c78cf8eff
Zhengnan Li <li.zhengn@northeastern.edu>
2025-03-31 15:04:06 -0400

    Address comments

--- examples/example_unpack.py
+++ examples/example_unpack.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     delay_axis = np.arange(unpacked.shape[0]) / fs_delay
     time_axis = np.arange(unpacked.shape[2]) / fs_time
     plt.pcolor(delay_axis * 1e3, time_axis, 20 * np.log10(np.abs(np.squeeze(unpacked[:, 0, :]))).T, vmin=-30, vmax=0)
+    plt.gca().invert_yaxis()  # Align with MATLAB
     plt.xlabel("Delay [ms]")
     plt.ylabel("Time [s]")
 
