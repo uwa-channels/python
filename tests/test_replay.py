59a887e2a9fe72dab8b0104bb4dc0c9dbb331fcf
Zhengnan Li <li.zhengn@northeastern.edu>
2025-04-21 14:56:10 -0400

    Add multichannel replay tests

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -49,11 +49,13 @@ def randsamples(population, num):
         {
             "channel_time": 5,
             "coeff": 2,
+            "d": 0.15,
             "fc": 10e3,
             "fs_delay": 8e3,
             "fs_time": 20,
             "has_f_resamp": False,
             "has_theta_hat": True,
+            "M": 4,
             "n_path": 8,
             "R": 4e3,
             "Tmp": 10e-3,
@@ -62,11 +64,13 @@ def randsamples(population, num):
         {
             "channel_time": 5,
             "coeff": 1,
+            "d": 0.15,
             "fc": 15e3,
             "fs_delay": 16e3,
             "fs_time": 20,
             "has_f_resamp": False,
             "has_theta_hat": True,
+            "M": 2,
             "n_path": 10,
             "R": 8e3,
             "Tmp": 20e-3,
@@ -75,11 +79,13 @@ def randsamples(population, num):
         {
             "channel_time": 5,
             "coeff": 1.5,
+            "d": 0.3,
             "fc": 15e3,
             "fs_delay": 8e3,
             "fs_time": 10,
             "has_f_resamp": False,
             "has_theta_hat": False,
+            "M": 4,
             "n_path": 8,
             "R": 4e3,
             "Tmp": 20e-3,
@@ -88,12 +94,14 @@ def randsamples(population, num):
         {
             "channel_time": 5,
             "coeff": 1.5,
+            "d": 0.2,
             "fc": 15e3,
             "fs_delay": 16e3,
             "fs_time": 10,
             "has_f_resamp": True,
             "has_theta_hat": False,
             "n_path": 8,
+            "M": 4,
             "R": 8e3,
             "Tmp": 20e-3,
             "velocity": -1,
@@ -101,11 +109,13 @@ def randsamples(population, num):
         {
             "channel_time": 5,
             "coeff": 1.5,
+            "d": 0.3,
             "fc": 15e3,
             "fs_delay": 10e3,
             "fs_time": 10,
             "has_f_resamp": True,
             "has_theta_hat": True,
+            "M": 2,
             "n_path": 8,
             "R": 6e3,
             "Tmp": 20e-3,
@@ -121,24 +131,31 @@ def test_replay_function(params):
     R = params["R"]
     channel_time = params["channel_time"]
     n_path = params["n_path"]
+    M = params["M"]
 
-    path_delay = np.sort(randsamples(np.arange(Tmp * 1e3), n_path)) / 1e3
+    path_delay = np.sort(randsamples(np.arange(Tmp * 1e3), n_path))[:, None] / 1e3
+    incremental_delay = np.arange(M)[None, :] * params["d"] / 1545
+    path_delay = path_delay + incremental_delay
     path_delay -= np.min(path_delay)
     path_gain = np.exp(-path_delay * params["coeff"] / Tmp)
     c_p = path_gain * np.exp(-1j * 2 * np.pi * fc * path_delay)
     h_hat = np.zeros(
         (
-            np.round(channel_time * fs_time).astype(int),
-            1,
+            M,
             np.ceil(fs_delay * Tmp * 1.5).astype(int),
         ),
         dtype=complex,
     )
-    h_hat[:, 0, np.round((path_delay + 0.2 * Tmp) * fs_delay).astype(int)] = np.tile(c_p, (h_hat.shape[0], 1))
+    rows = np.tile(np.arange(M), n_path)
+    cols = np.round((path_delay + 0.2 * Tmp) * fs_delay).astype(int).ravel()
+    h_hat[rows, cols] = c_p.ravel()
+    h_hat = np.tile(h_hat[None, :, :], (np.round(channel_time * fs_time).astype(int), 1, 1))
+
     f_resamp = 1 / (1 + params["velocity"] / 1545)
     a = 1 - 1 / f_resamp
     t = np.arange(np.round(channel_time * fs_delay).astype(int))
     theta_hat = -a * 2 * np.pi * fc * t[:, None] / fs_delay
+    theta_hat = np.tile(theta_hat, (1, M))
 
     channel = {
         "h_hat": {"real": np.real(h_hat), "imag": np.imag(h_hat)},
@@ -163,37 +180,38 @@ def test_replay_function(params):
     passband = np.real(baseband * np.exp(2j * np.pi * fc * np.arange(len(baseband)) / fs))
     input_signal = np.concatenate((np.zeros(1000), passband, np.zeros(1000)))
 
-    r = replay(input_signal, fs, [0], channel, 1)
-    v = r[:, 0] * np.exp(-2j * np.pi * fc * np.arange(len(r)) / fs)
+    r = replay(input_signal, fs, np.arange(M), channel, 1)
+    sync = np.zeros((M,))
+    for m in range(M):
+        v = r[:, m] * np.exp(-2j * np.pi * fc * np.arange(len(r)) / fs)
 
-    frac = Fraction(f_resamp).limit_denominator()
-    baseband_resampled = baseband * np.exp(-2j * a * np.pi * fc * np.arange(len(baseband)) / fs)
-    baseband_resampled = sg.resample_poly(baseband_resampled, frac.numerator, frac.denominator)
+        frac = Fraction(f_resamp).limit_denominator()
+        baseband_resampled = baseband * np.exp(-2j * a * np.pi * fc * np.arange(len(baseband)) / fs)
+        baseband_resampled = sg.resample_poly(baseband_resampled, frac.numerator, frac.denominator)
 
-    xcor = np.abs(sg.fftconvolve(v, baseband_resampled[::-1].conj()))
-    xcor = xcor / np.max(xcor)
+        xcor = np.abs(sg.fftconvolve(v, baseband_resampled[::-1].conj()))
+        xcor = xcor / np.max(xcor)
 
-    peaks, _ = sg.find_peaks(
-        xcor,
-        height=np.min(np.abs(path_gain)) * 0.8,
-        distance=np.min(np.diff(np.sort(path_delay))) * fs * 0.9,
-    )
+        peaks, _ = sg.find_peaks(
+            xcor,
+            height=np.min(np.abs(path_gain[:, m])) * 0.8,
+            distance=np.min(np.diff(np.sort(path_delay[:, m]))) * fs * 0.9,
+        )
+        sync[m] = peaks[0]
 
-    estimated_gain = xcor[peaks]
-    peaks -= np.min(peaks)
-    estimated_delays = peaks / fs
+        estimated_gain = xcor[peaks]
+        peaks -= np.min(peaks)
+        estimated_delays = peaks / fs
 
-    # import matplotlib.pyplot as plt
-
-    # lags = np.arange(len(xcor)) - np.argmax(xcor)
-    # plt.stem(path_delay * 1e3, path_gain, markerfmt="x", basefmt=" ")
-    # plt.plot(lags / fs * 1e3, xcor)
-    # plt.xlim([np.min(estimated_delays) * 1e3 - 5, np.max(estimated_delays) * 1e3 + 10])
-    # plt.xlabel("Delay [ms]")
+        # import matplotlib.pyplot as plt
+        # lags = np.arange(len(xcor)) - np.argmax(xcor)
+        # plt.stem(path_delay[:, m] * 1e3, path_gain[:, m], markerfmt="x", basefmt=" ")
+        # plt.plot((lags+sync[m]-sync[0]) / fs * 1e3, xcor)
+        # plt.xlim([np.min(estimated_delays) * 1e3 - 5, np.max(estimated_delays) * 1e3 + 10])
+        # plt.xlabel("Delay [ms]")
+        criteria = np.abs(np.sum(path_delay[:, m] * path_gain[:, m]) - np.sum(estimated_delays * estimated_gain))
+        assert criteria < 3e-4 * n_path, f"Test criteria failed: {criteria:.3e}"
     # plt.show()
-
-    criteria = np.abs(np.sum(path_delay * path_gain) - np.sum(estimated_delays * estimated_gain))
-    assert criteria < 3e-4 * n_path, f"Test criteria failed: {criteria:.3e}"
 
 
 if __name__ == "__main__":
